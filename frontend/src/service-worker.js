f6e8435187514ee275353e5b1f000c2ab08233c5
Vincent Lark <v.lark@astonai.com>
2025-06-04 10:20:47 +0200

    chore: test web push

--- frontend/src/service-worker.js
+++ frontend/src/service-worker.js
@@ -1,15 +1,13 @@
 /// <reference types="@sveltejs/kit" />
 
-console.log('dzdzad');
+self.addEventListener('push', event => {
+  console.log('[Service Worker] Push Received.');
+  console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
 
-// self.addEventListener('push', event => {
-//   console.log('[Service Worker] Push Received.');
-//   console.log(`[Service Worker] Push had this data: "${event.data.text()}"`);
+  const title = 'Test Webpush';
+  const options = {
+    body: event.data.text(),
+  };
 
-//   const title = 'Test Webpush';
-//   const options = {
-//     body: event.data.text(),
-//   };
-
-//   event.waitUntil(self.registration.showNotification(title, options));
-// });
+  event.waitUntil(self.registration.showNotification(title, options));
+});
